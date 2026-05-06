ae9c4f4cd6b8819f4648b64167118eeb4c7f6fc9
66tomvvs <inostrozatomas100@gmail.com>
2026-05-06 10:22:53 -0400

    2

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Producto
 # Create your views here.
+
+def catalogo_view(request):
+    productos = Producto.objects.all()
+    
+    contexto = {
+        'productos': productos
+    }
+    
+    return render(request, 'catalogo/inicio.html', contexto)
