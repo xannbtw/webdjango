0b3fa97ffcdf06c8d87bf5add97e7299a40db4ae
66tomvvs <inostrozatomas100@gmail.com>
2026-05-10 16:51:03 -0400

    'diseño'

--- web/settings.py
+++ web/settings.py
@@ -134,8 +134,7 @@ DEBUG = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
