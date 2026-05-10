8893b066c3966a56447c5e7bc8bd41a2bebd5761
66tomvvs <inostrozatomas100@gmail.com>
2026-05-10 19:42:06 -0400

    fix

--- web/settings.py
+++ web/settings.py
@@ -7,7 +7,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ob(qfz#v(1&%bm^=j!o6+^6n(nf^2jk23e!!c5cu)xkf+i!=5p')
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
