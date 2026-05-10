1a17c32376e363be22e173b69c4990fbf3d467d9
66tomvvs <inostrozatomas100@gmail.com>
2026-05-10 16:56:27 -0400

    redy

--- web/settings.py
+++ web/settings.py
@@ -134,7 +134,6 @@ DEBUG = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -150,3 +149,8 @@ CLOUDINARY_STORAGE = {
 }
 # Guarda /media/ en Cloudinary
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'catalogo', 'static'),
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
