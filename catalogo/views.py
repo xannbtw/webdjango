95638a5003317963a76b1e6083863c47e60c5f8a
yohel <yohelignacio21@gmail.com>
2026-05-13 00:54:45 -0400

    feat: login, logout y registro de usuarios

--- catalogo/views.py
+++ catalogo/views.py
@@ -81,3 +81,18 @@ def ver_carrito(request):
         'total_carrito': total_carrito,
     }
     return render(request, 'catalogo/carrito.html', context)
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+
+def registro(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('inicio')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/registro.html', {'form': form})
--- web/urls.py
+++ web/urls.py
@@ -18,15 +18,22 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import serve
+from django.contrib.auth import views as auth_views
+from catalogo.views import registro
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalogo.urls')),
-    
 ]
 
 urlpatterns += [
     re_path(r'^media/(?P<path>.*)$', serve, {
         'document_root': settings.MEDIA_ROOT,
     }),
+]
+
+urlpatterns += [
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('registro/', registro, name='registro'), 
 ]
